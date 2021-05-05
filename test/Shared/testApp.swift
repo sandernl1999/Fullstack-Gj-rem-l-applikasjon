d2035da3572167fc61b799f2e384d924a79e01a4
Sander Nordby Larsen <sanderlarsen@Sanders-MacBook-Pro-2.local>
2021-05-05 15:03:03 +0200

    Upwork

---
+++ test/Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Sander Nordby Larsen on 13/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
